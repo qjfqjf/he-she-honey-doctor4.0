429f3f3fd2ee0bedb29ed1df460e0624ec36a874
Thexingmeng <1920238209@qq.com>
2023-09-01 20:39:49 +0800

    打包前的版本

---
+++ unpackage/cache/wgt/__UNI__23859FB/static/js/homePage/staticData.js
@@ -0,0 +1,145 @@
+export const wisperImage= [
+	'/static/icon/homePage/swiperImg2.png',
+    '/static/icon/homePage/swiperImg1.png',
+	'/static/icon/homePage/swiperImg4.png',
+]
+export const   appManage= [
+    {
+        name: "健康检测",
+        icon: "/static/icon/homePage/healthMonitor.png",
+        path: "/pages/healthMonitor/index"
+    },
+	{
+	    name: "健康评估",
+	    icon: "/static/icon/homePage/healthAssessment.png",
+	    path: "/pages/healthEstimate/index"
+	},
+	{
+	    name: "诊断/调理",
+	    icon: "/static/icon/homePage/HealthAdvisory.png",
+	    path: "/pages/healthTreatment/index"
+	},
+	{
+	    name: "AI智能",
+	    icon: "/static/icon/homePage/AI.png",
+	    //path: "/pages/professionalCommunity/index"
+	},
+    {
+        name: "专业社区",
+        icon: "/static/icon/homePage/healthRecords.png",
+        path: "/pages/healthFile/index"
+    },
+    {
+        name: "工单统筹",
+        icon: "/static/icon/homePage/applicationService.png",
+        path: "/pages/WorkOrderCoordination/WorkOrderCoordination"
+    },
+
+    {
+        name: "监管预警",
+        icon: "/static/icon/homePage/fengxian.png", 
+        //path: "/pages/treat/index"
+    },
+
+    {
+        name: "教育/培训",
+        icon: "/static/icon/homePage/educate.png",
+        //path: "/pages/educate/index"
+    },
+
+
+
+
+]
+export const  appFeature= [
+    [{
+        name: "医疗",
+        icon: "static/icon/homePage/medical.png",
+        path: "/pages/educate/index"
+    },
+    {
+        name: "保健",
+        icon: "static/icon/homePage/healthCare.png",
+         path: "/pages/educate/index"
+
+    },
+    {
+        name: "体检",
+        icon: "static/icon/homePage/physicalExamination.png",
+         path: "/pages/educate/index"
+
+    },
+    {
+        name: "心理",
+        icon: "static/icon/homePage/psychology.png",
+         path: "/pages/educate/index"
+    },
+    {
+        name: "美容",
+        icon: "static/icon/homePage/cosmetic.png",
+        path: "/pages/educate/index"
+    }],
+	[{
+	    name: "健身",
+	    icon: "static/icon/homePage/jianshen.png",
+	    path: "/pages/educate/index"
+	},
+	{
+	    name: "养老",
+	    icon: "static/icon/homePage/yanglao.png",
+	    path: "/pages/educate/index"
+	},
+	{
+	    name: "助残",
+	    icon: "static/icon/homePage/zhucan.png",
+	    path: "/pages/educate/index"
+	},
+	{
+	    name: "商城",
+	    icon: "static/icon/homePage/shangcheng.png",
+	    path: "/pages/educate/index"
+	},
+	{
+	    name: "直播",
+	    icon: "static/icon/homePage/zhibo.png",
+	    path: "/pages/educate/index"
+	}]
+]
+export const homePageIcons = {
+    Scanning:{
+        name: "扫描二维码",
+        icon: "static/icon/homePage/Scanning.png"
+    },
+    Location:{
+        name: "位置",
+        icon: "static/icon/homePage/Location.png"
+    },
+    Glu:{
+        name: "高压",
+        icon: "static/icon/homePage/Glu.png"
+    },
+    Bp:{
+        name: "低压",
+        icon: "static/icon/homePage/Bp.png"
+    },
+    UricAcid:{
+        name: "血尿素",
+        icon: "static/icon/homePage/UricAcid.png"
+    },
+    shop:{
+        name: "商城",
+        icon: "static/icon/homePage/home.png"
+    },
+    live:{
+        name: "直播",
+        icon: "static/icon/homePage/home.png"
+    },
+    ArrowUp:{
+        name: "向上箭头",
+        icon: "static/icon/homePage/ArrowUp.png"
+    },
+    ArrowDown:{
+        name: "向下箭头",
+        icon: "static/icon/homePage/ArrowDown.png"
+    }
+    }
